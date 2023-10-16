5f35824ed02d0d27d0e64f888f0ebe86062993fa
jirlaynnedhamylle <139500386+jirlaynnedhamylle@users.noreply.github.com>
2023-10-16 00:18:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function jsStyle() {
+    var paragraph = document.getElementById('text');
+
+    // Alterando o estilo do parágrafo
+    paragraph.style.fontFamily = 'Arial';
+    paragraph.style.fontSize = '20px';
+    paragraph.style.color = 'blue';
+}
